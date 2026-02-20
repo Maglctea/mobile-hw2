9bb8f10a06572c40384c1ee840e09e2bcc45c7bb
Magictea <sherbakovdaniil6@gmail.com>
2026-02-20 21:16:47 +0300

    complete homework

--- composeApp/src/commonMain/kotlin/com/example/mobile_hw2/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/mobile_hw2/App.kt
@@ -1,49 +1,39 @@
 package com.example.mobile_hw2
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import org.jetbrains.compose.resources.painterResource
-
-import mobilehw2.composeapp.generated.resources.Res
-import mobilehw2.composeapp.generated.resources.compose_multiplatform
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.mobile_hw2.ui.theme.AppTheme
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
+    val navController = rememberNavController()
+
+    AppTheme {
+
+        NavHost(
+            navController = navController,
+            startDestination = "welcome",
             modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
                 .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+            composable("welcome") {
+                WelcomeScreen(onToLogin = { navController.navigate("login") })
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+            composable("login") {
+                LoginScreen(onBack = { navController.navigateUp() })
             }
         }
+
     }
 }
---
+++ composeApp/src/commonMain/kotlin/com/example/mobile_hw2/config/AppConfig.kt
@@ -0,0 +1,6 @@
+package com.example.mobile_hw2.config
+
+object AppConfig {
+    const val WELCOME_ICON_URL = "https://psv4.userapi.com/s/v1/d2/GmizTQNL8XH6lXtstV196w_6hKEJhME6agayjkmVwVIXMGEq1Ri_w8p6TQH_8FQdlaiLV--BDRATwoNrRxs1xHVbSaBB5wnLBPbrV-njhfXAYmirv_qDMhU3nbBBr1l04xQNM1-oy2ri/welcome-icon.png"
+    const val LOGO_ICON_URL = "https://psv4.userapi.com/s/v1/d2/h1mCUYqViZki2gkkin5xQ8HxBXuLED2O6ulOk-4fxjGnes8nlrub3ZKPZiuKfvI9zZXz0eyFD_wc33cqTFxQcD-GcKPNzKnillaAkd6MIhezMwA_5R1JRqil3a48n3NgBGGg1QzdhlDd/logo.png"
+}
